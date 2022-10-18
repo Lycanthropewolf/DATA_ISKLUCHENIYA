64494ca8b94649e914ffa172335ee266685a29d0
Антон <antonolegovich2017@icloud.com>
2022-10-18 22:50:27 +0600

    sda

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean succes=DATA.validate("on","on","on");
+        if (succes){
+            System.out.println(" Данные верны");
+        }else {
+            System.out.println(" Данные не верны");
+        }
+
+
+
+    }
+
+
+
+
+
+}
